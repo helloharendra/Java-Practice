1ad9dbf39c45c3ab47c5c7fbe86e9acbb35de038
Harendra Prajapati <harendraprajapati72@gmail.com>
2023-07-12 20:09:55 +0530

    practice1

---
+++ practice1.java
@@ -0,0 +1,13 @@
+/**
+ * practice1
+ */
+public class practice1 {
+  
+    public static void main(String[] args) {
+        int num=5;
+        for(int i=1;i<=10;i++){
+            System.out.println(num*i);
+
+        }
+    }
+}
