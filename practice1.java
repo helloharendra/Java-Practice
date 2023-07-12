8bdb6d9cd0d37d1ef5a7a13e8106d3df652be90d
Harendra Prajapati <harendraprajapati72@gmail.com>
2023-07-13 00:12:08 +0530

    sum of n natural numbers

--- practice1.java
+++ practice1.java
@@ -1,10 +1,15 @@
+import java.util.Scanner;
+
 /**
  * practice1
  */
 public class practice1 {
   
     public static void main(String[] args) {
-        int num=5;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("please Enter a number");
+        int num=sc.nextInt();
+        
         for(int i=1;i<=10;i++){
             System.out.println(num +"*"+i+"=" + num*i);
 
