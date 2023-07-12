048ef4e7adf9cb25953e7dbb006ce9ff4a7e0765
Harendra Prajapati <harendraprajapati72@gmail.com>
2023-07-12 20:14:57 +0530

    commit

--- practice1.java
+++ practice1.java
@@ -6,7 +6,7 @@ public class practice1 {
     public static void main(String[] args) {
         int num=5;
         for(int i=1;i<=10;i++){
-            System.out.println(num*i);
+            System.out.println(num +"*"+i+"=" + num*i);
 
         }
     }
